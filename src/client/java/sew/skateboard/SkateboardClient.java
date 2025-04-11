37529633e4bb2023015ec395ec243a555b2d9f4c
ivy <justinalextrem@gmail.com>
2025-04-11 15:41:53 +0200

    first commit

---
+++ src/client/java/sew/skateboard/SkateboardClient.java
@@ -0,0 +1,10 @@
+package sew.skateboard;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SkateboardClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
