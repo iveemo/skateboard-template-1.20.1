b30000e1bc12ad3b41be17676054609ecd1cf195
Eduard <daneduardleuska@gmail.com>
2025-06-03 14:00:15 +0200

    feat: removed useless double folder, renewed important classes in the actual main module and version is 1.1 now

--- src/client/java/sew/skateboard/SkateboardClient.java
+++
@@ -1,9 +0,0 @@
-package sew.skateboard;
-
-import net.fabricmc.api.ClientModInitializer;
-
-public class SkateboardClient implements ClientModInitializer {
-	@Override
-	public void onInitializeClient() {
-		}
-}
---
+++ src/main/java/sew/skateboard/SkateboardModDataGenerator.java
@@ -0,0 +1,11 @@
+package sew.skateboard;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SkateboardModDataGenerator implements DataGeneratorEntrypoint {
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+    }
+}
